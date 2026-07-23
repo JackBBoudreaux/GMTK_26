b4aeb5b8ceef17ff6c14c6d1ec6b50a4f4aa6288
Jack Boudreaux <jackboudreaux13@gmail.com>
2026-07-23 17:02:53 -0400

    Made the character art the new sprites and added the animations
    I also made the bullets shoot a lil faster

--- Assets/Scripts/Player_movement.gd
+++ Assets/Scripts/Player_movement.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
- 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+
 #player basic movement
 const MAX_SPEED: int = 250
 const ACCELERATION: int = 1400
@@ -9,20 +11,28 @@ const FRICTION: int = 1000
 const DODGE_SPEED: float = 400.0
 const DODGE_DURATION: float = 0.3
 
+#dodge roll stuff
 var dodge_roll_dir: Vector2 = Vector2. ZERO
 var dodge_roll_timer: float = 0.0
 
 var can_take_damage: bool = true
 
  
-@onready var sprite: Sprite2D = $Sprite2D
+func _physics_process(_delta: float) -> void:
+	if velocity.x > 1 or velocity.x < -1 or velocity.y > 0 or velocity.y < -1:
+		animated_sprite_2d.animation = "running"
+	else:
+		animated_sprite_2d.animation = "idle"
+
+
  
+
  #flips player sprite 
 func _process(delta: float) -> void:
 	if get_global_mouse_position().x < global_position.x:
-		sprite.flip_h = true
+		animated_sprite_2d.flip_h = true
 	else:
-		sprite.flip_h = false
+		animated_sprite_2d.flip_h = false
  
 	var direction = Input.get_vector("left", "right", "up", "down")
 	if direction:
