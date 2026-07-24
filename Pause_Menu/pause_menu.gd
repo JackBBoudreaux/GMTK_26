f2e251caf76b93c790c1282b048ca2888070b4f0
scarpatovic <scarpatovic@gmail.com>
2026-07-24 16:02:02 -0400

    Main AND Pause menu
    Added Main Menu (Start & Exit)
    
    Added Pause Menu (Resume & Exit)
    
    Settings is still a WIP

---
+++ Pause_Menu/pause_menu.gd
@@ -0,0 +1,22 @@
+extends CanvasLayer
+
+func _ready() -> void:
+	visible = false
+	get_tree().paused = false
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("open_pause_menu"):
+		if get_tree().paused:
+			visible = false
+			get_tree().paused = false
+		else:
+			visible = true
+			get_tree().paused = true
+		
+func _on_RESUME_pressed() -> void:
+	visible = false
+	get_tree().paused = false
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://main_menu.tscn")
