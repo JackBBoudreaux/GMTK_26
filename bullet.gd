a7e59ea4671ec045d3c6c1d9c9cfd9090c20c294
Jack Boudreaux <jackboudreaux13@gmail.com>
2026-07-25 11:37:24 -0400

    Made the enemies follow the player

--- bullet.gd
+++ bullet.gd
@@ -10,3 +10,11 @@ func _process(delta: float) -> void:
  #when the bullet exits the screen it gets deleted from memory
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
+
+
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("enemy"):
+		body.queue_free()
+		queue_free()
--- enemy.gd
+++ enemy.gd
@@ -1,30 +1,21 @@
 extends CharacterBody2D
-
-
-var speed = 280
+var speed = 180
 var player_pos
 var target_pos
-@onready var Player: CharacterBody2D = $"../../Player"
+@onready var player: CharacterBody2D = $"../../Player"
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
-
-
-
-
 func _physics_process(delta: float) -> void:
-	player_pos = Player.position
-	target_pos = (player_pos - position).normalized()
+	player_pos = player.global_position
+	target_pos = (player_pos - global_position).normalized()
 
-	if position.distance_to(player_pos) > 3:
-		position += target_pos * speed * delta
-
-
-func _process(delta: float) -> void:
-	if player_pos < -1:
+	if global_position.x > player_pos.x:
 		animated_sprite_2d.flip_h = true
 	else:
 		animated_sprite_2d.flip_h = false
 
+	if global_position.distance_to(player_pos) > 3:
+		global_position += target_pos * speed * delta
 
 func _on_health_health_depleted():
 	queue_free()
