d712bdde201166f2e3493d2124dc64c72456bfa2
scarpatovic <scarpatovic@gmail.com>
2026-07-26 00:16:12 -0400

    Added sounds to Main Menu

--- Main_Menu/Script/main_menu.gd
+++ Main_Menu/Script/main_menu.gd
@@ -13,19 +13,40 @@ func _ready():
 	credits.visible = false
 
 func _on_START_pressed() -> void:
+	$StartButtonSound.play()
+	await get_tree().create_timer(4.0).timeout
 	get_tree().change_scene_to_file("res://Main.tscn")
 	
 func _on_SETTINGS_pressed() -> void:
+	$ClickSound.play()
 	print("settings pressed")
 
 func _on_CREDITS_pressed() -> void:
 	print("credits pressed")
+	$ClickSound.play()
+	$CreditSound.play()
 	main_buttons.visible = false
 	credits.visible = true
 	
 func _on_X_pressed() -> void:
+	$CreditSound.stop()
 	main_buttons.visible = true
 	credits.visible = false
 	
 func _on_EXIT_pressed() -> void:
+	$ClickSound.play()
 	get_tree().quit()
+
+func _on_START_mouse_entered() -> void:
+	$HoverSound.play()
+
+func _on_SETTINGS_mouse_entered() -> void:
+	$HoverSound.play()
+
+
+func _on_CREDITS_mouse_entered() -> void:
+	$HoverSound.play()
+
+
+func _on_EXIT_mouse_entered() -> void:
+	$HoverSound.play()
