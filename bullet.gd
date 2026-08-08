ea9b39ca820634d501774116b4e795d495fc231d
Jack Boudreaux <jackboudreaux13@gmail.com>
2026-08-08 18:55:31 -0400

    made bullets collide with walls

--- bullet.gd
+++ bullet.gd
@@ -10,3 +10,6 @@ func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 func _on_hit_box_area_entered(area: Area2D) -> void:
 	if area is HurtBox:
 		queue_free()
+
+func _on_hit_box_body_entered(body: Node2D) -> void:
+	queue_free()
